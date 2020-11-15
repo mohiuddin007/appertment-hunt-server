db237bc77fb7fad5ee6abe980cf62dcd7eedf55c
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-11-15 16:27:36 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config()
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l47bs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const app = express()
 app.use(bodyParser.json());
 app.use(cors());
+
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const appertmentsCollection = client.db(`${process.env.DB_NAME}`).collection("AllAppertments");
+  console.log("database connected");
+});
+
 
 app.get('/', function (req, res) {
   res.send('hello world')
